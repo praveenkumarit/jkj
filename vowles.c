b3e3db31f9d248cc149db15ee9d798d83088aba8
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-01-24 16:21:40 +0530

    Create vowles.c

---
+++ vowles.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))
+{
+printf("%d is vowles ",a);
+else
+printf("in is consonant");
+}
+return 0;
+}
