030014583d86acb3e64bb02f156f665bed29ca72
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-02-12 15:44:23 +0530

    Create countno.c

---
+++ countno.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()#include<stdio.h>
+void main()
+{
+    int a=0,b,c;
+    b=123;
+    c=b;
+    while(b!=0)
+    {
+        b=b/10;
+        a++;
+        
+    }
+    printf("%d",a);
+}
+​
+17
+{
+18
+    int a=0,b,c;
+19
+    b=123;
+20
+  
