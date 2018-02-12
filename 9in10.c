1d8194180998801b97f59a7b099f9a02a061f7c8
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-02-12 15:40:10 +0530

    Update 9in10.c

--- 9in10.c
+++ 9in10.c
@@ -5,18 +5,7 @@ void main()
     for(i=0;i<=10;i++)
     {
         if(i==9#include<stdio.h>
-void main()
-{
-    int i;
-    for(i=0;i<=10;i++)
-    {
-        if(i==9)
-        printf("yes");
-    }
-}
 
-)
         printf("yes");
-    }
-}
-
+           }
+           }
