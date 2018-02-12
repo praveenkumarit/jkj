a9b517ad4192b65a6cfb4773e38d21404d680505
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-02-12 15:39:19 +0530

    Create 9in10.c

---
+++ 9in10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0;i<=10;i++)
+    {
+        if(i==9#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0;i<=10;i++)
+    {
+        if(i==9)
+        printf("yes");
+    }
+}
+
+)
+        printf("yes");
+    }
+}
+
