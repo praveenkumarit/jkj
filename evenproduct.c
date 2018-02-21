ec03c2dd1f6aee00803188f12a26d38ba789e6fc
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-02-21 16:06:01 +0530

    Rename  even.c to  evenproduct.c

---
+++ evenproduct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=12,b=3,c;
+    c=a*b;
+    if(c%2==0)
+    {
+        printf("even");
+    }
+        else
+        {
+        printf("odd");
+    }
+}
+
