0810174b3839bbcf9efcdc20c6a06b73ee880e31
praveenkumarit <30828629+praveenkumarit@users.noreply.github.com>
2018-02-26 16:03:18 +0530

    Create arm interval.c

---
+++ arm interval.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int c,sum=0,i,a,b;
+    a=150;
+    b=160;
+    for(i=a;i<=b;i++)
+    while(a!=0)
+    {
+        c=a%10;
+        sum=sum+a*a*a;
+        a=a/10;
+        
+    }
+    printf("%d",sum);
+ }
